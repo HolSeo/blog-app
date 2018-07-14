20ee11e5f2cb935255c804ee8e3a70146dee0f96
HolSeo <dseo1004@gmail.com>
2018-07-13 22:23:29 -0400

    styled readPage

--- src/components/ReadBlogPage.js
+++ src/components/ReadBlogPage.js
@@ -6,10 +6,16 @@ export class ReadBlogPage extends React.Component {
     render() {
         return (
             <div>
-                <h1>{this.props.blog.title}</h1>
-                <p>{this.props.blog.body}</p>
-                <p>{this.props.blog.name}</p>
-                <footer>{moment(this.props.blog.createdAt).format('MMMM Do YYYY')}</footer>
+                <div className="page-header">
+                    <div className="content-container">
+                        <h1 className="title">{this.props.blog.title}</h1>
+                        <h2 className="title">By {this.props.blog.name}</h2>
+                        <h3 className="title">{moment(this.props.blog.createdAt).format('MMMM Do YYYY')}</h3>
+                    </div>
+                </div>
+                <div className="content-container">
+                    <p>{this.props.blog.body}</p>
+                </div>
             </div>
         )
     }
